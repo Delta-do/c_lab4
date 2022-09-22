4d3df520c61792c522b122c4f5e4869e2c1a746b
Delta-do <pumponisharf@mail.ru>
2022-09-22 10:33:48 +0300

    Initial commit

---
+++ c_lab4/Source.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <locale.h>
+
+void t1()
+{
+	char c = ' ';
+	int i = 0;
+	float f = 0;
+	double d = 0;
+	printf("Введите символ "); 
+	scanf_s("%c", &c);
+	printf("Введите целое число ");
+	scanf_s("%d", &i);
+	printf("Введите дробное число ");
+	scanf_s("%f", &f);
+	printf("Введите второе дробное число ");
+	scanf_s("%lf", &d);
+
+	puts("----------");
+	printf("%c\n%d\n%f\n%lf\n", c, i, f, d);
+	puts("----------");
+	printf("Целая часть float: %d\nДробная часть float: %.3f\n", (int)f, f - (int)f);
+	printf("Код введённого символа: %d\n", (int)c);
+	printf("Число 1/i: %.3f\n", 1. / i);
+}
+
+void t2() 
+{
+	int a = 11;
+	int b = 3;
+	int x;
+	float y;
+	double z;
+	x = a / b;
+	y = (float) a / b;
+	z = (double) a / b;
+	printf("int: %d\nfloat: %f\ndouble: %lf", x, y, z);
+}
+
+void t3()
+{
+	int n = 0;
+	printf("Введите целое трёхзначное число ");
+	scanf_s("%d", &n);
+	int n1 = n / 100;
+	int n2 = n % 100 / 10;
+	int n3 = n % 10;
+	printf("Последняя цифра: %d\nПервая цифра: %d\nСумма цифр: %d\n", n3, n1, n1 + n2 + n3);
+	printf("Число наоборот: %d", n3 * 100 + n2 * 10 + n1);
+}
+
+void main()
+{
+	setlocale(LC_ALL, "RUS");
+
+	//1 задание
+	//t1();
+
+	//2 задание
+	//t2();
+
+	//3 задание
+	t3();
+	
+}
